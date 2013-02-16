a68cdb7a0b34830fe849371c95b0dc8938a9f28d
Siddhartha Agrawal <agrawals@codeaurora.org>
2013-02-15 20:32:52 -0800

    msm_fb: display: Send pixel data while sending on/off commands
    To avoid LCD off sequence violeation for some LCD vendors,
    we need to keep sending video data during off/sleep commands.
    This fixes issues seen while going to a suspend.
    
    Crs-Fixed: 445707
    Change-Id: I0ba2704502e8959d0f82deea60481cd5b1f0be20
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -556,6 +556,19 @@ static void mdp4_dsi_video_tg_off(struct vsycn_ctrl *vctrl)
 	msleep(20);
 }
 
+int mdp4_dsi_video_splash_done(void)
+{
+	struct vsycn_ctrl *vctrl;
+	int cndx = 0;
+
+	vctrl = &vsync_ctrl_db[cndx];
+
+	mdp4_dsi_video_tg_off(vctrl);
+	mipi_dsi_controller_cfg(0);
+
+	return 0;
+}
+
 int mdp4_dsi_video_on(struct platform_device *pdev)
 {
 	int dsi_width;
@@ -657,15 +670,6 @@ int mdp4_dsi_video_on(struct platform_device *pdev)
 	}
 
 	atomic_set(&vctrl->suspend, 0);
-
-	if (!(mfd->cont_splash_done)) {
-		mfd->cont_splash_done = 1;
-		mdp4_dsi_video_tg_off(vctrl);
-		mipi_dsi_controller_cfg(0);
-		/* Clks are enabled in probe.
-		   Disabling clocks now */
-		mdp_clk_ctrl(0);
-	}
 
 	pipe->src_height = fbi->var.yres;
 	pipe->src_width = fbi->var.xres;
