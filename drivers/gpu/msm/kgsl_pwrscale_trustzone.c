6bce0fa5cc5a0e95e9de071cc96bbe431125ce2b
Nikez <nikez141@gmail.com>
2014-01-12 22:27:35 +0100

    drivers: gpu: update to .38
    Signed-off-by: spezi77 <spezi77@gmx.com>
    
    Conflicts:
    	arch/arm/mach-msm/board-msm7x27a-regulator.h
    	arch/arm/mach-msm/devices-msm7x2xa.h
    	arch/arm/mach-msm/include/mach/qdsp5v2/pcm_funcs.h
    	drivers/gpu/msm/Makefile
    	drivers/gpu/msm/a2xx_reg.h
    	drivers/gpu/msm/a3xx_reg.h
    	drivers/gpu/msm/adreno.c
    	drivers/gpu/msm/adreno.h
    	drivers/gpu/msm/adreno_a2xx.c
    	drivers/gpu/msm/adreno_a3xx.c
    	drivers/gpu/msm/adreno_a3xx_trace.c
    	drivers/gpu/msm/adreno_debugfs.c
    	drivers/gpu/msm/adreno_debugfs.h
    	drivers/gpu/msm/adreno_drawctxt.c
    	drivers/gpu/msm/adreno_drawctxt.h
    	drivers/gpu/msm/adreno_pm4types.h
    	drivers/gpu/msm/adreno_postmortem.c
    	drivers/gpu/msm/adreno_ringbuffer.c
    	drivers/gpu/msm/adreno_ringbuffer.h
    	drivers/gpu/msm/adreno_snapshot.c
    	drivers/gpu/msm/kgsl.c
    	drivers/gpu/msm/kgsl.h
    	drivers/gpu/msm/kgsl_cffdump.c
    	drivers/gpu/msm/kgsl_debugfs.c
    	drivers/gpu/msm/kgsl_debugfs.h
    	drivers/gpu/msm/kgsl_device.h
    	drivers/gpu/msm/kgsl_drm.c
    	drivers/gpu/msm/kgsl_gpummu.c
    	drivers/gpu/msm/kgsl_iommu.c
    	drivers/gpu/msm/kgsl_mmu.c
    	drivers/gpu/msm/kgsl_mmu.h
    	drivers/gpu/msm/kgsl_pwrctrl.c
    	drivers/gpu/msm/kgsl_pwrctrl.h
    	drivers/gpu/msm/kgsl_pwrscale.c
    	drivers/gpu/msm/kgsl_pwrscale.h
    	drivers/gpu/msm/kgsl_pwrscale_idlestats.c
    	drivers/gpu/msm/kgsl_pwrscale_trustzone.c
    	drivers/gpu/msm/kgsl_sharedmem.c
    	drivers/gpu/msm/kgsl_sharedmem.h
    	drivers/gpu/msm/kgsl_snapshot.c
    	drivers/gpu/msm/kgsl_sync.c
    	drivers/gpu/msm/kgsl_sync.h
    	drivers/gpu/msm/kgsl_trace.h
    	drivers/gpu/msm/z180.c
    	drivers/gpu/msm/z180.h
    	drivers/gpu/msm/z180_reg.h
    	include/linux/msm_kgsl.h
    	include/linux/msm_q6vdec.h
    	include/linux/qcomwlan7x27a_pwrif.h

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2011, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,7 +13,6 @@
  * 2013-06-20: Added KGSL Simple GPU Governor
  */
 
-#include <linux/export.h>
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/io.h>
@@ -34,14 +33,9 @@ struct tz_priv {
 	int governor;
 	unsigned int no_switch_cnt;
 	unsigned int skip_cnt;
-	struct kgsl_power_stats bin;
 };
 spinlock_t tz_lock;
 
-/* FLOOR is 5msec to capture up to 3 re-draws
- * per frame for 60fps content.
- */
-#define FLOOR			5000
 #define SWITCH_OFF		200
 #define SWITCH_OFF_RESET_TH	40
 #define SKIP_COUNTER		500
@@ -87,20 +81,26 @@ static ssize_t tz_governor_store(struct kgsl_device *device,
 				struct kgsl_pwrscale *pwrscale,
 				 const char *buf, size_t count)
 {
+	char str[20];
 	struct tz_priv *priv = pwrscale->priv;
 	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
+	int ret;
+
+	ret = sscanf(buf, "%20s", str);
+	if (ret != 1)
+		return -EINVAL;
 
 	mutex_lock(&device->mutex);
 
-	if (!strncmp(buf, "ondemand", 8))
+	if (!strncmp(str, "ondemand", 8))
 		priv->governor = TZ_GOVERNOR_ONDEMAND;
 	else if (!strncmp(buf, "simple", 6))
 		priv->governor = TZ_GOVERNOR_SIMPLE;
-	else if (!strncmp(buf, "performance", 11))
+	else if (!strncmp(str, "performance", 11))
 		priv->governor = TZ_GOVERNOR_PERFORMANCE;
 
 	if (priv->governor == TZ_GOVERNOR_PERFORMANCE)
-		kgsl_pwrctrl_pwrlevel_change(device, pwr->max_pwrlevel);
+		kgsl_pwrctrl_pwrlevel_change(device, pwr->thermal_pwrlevel);
 
 	mutex_unlock(&device->mutex);
 	return count;
@@ -121,10 +121,11 @@ static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
 	struct tz_priv *priv = pwrscale->priv;
 	if (device->state != KGSL_STATE_NAP &&
-		priv->governor == TZ_GOVERNOR_ONDEMAND ||
-		priv->governor == TZ_GOVERNOR_SIMPLE)
+		(priv->governor == TZ_GOVERNOR_ONDEMAND ||
+		priv->governor == TZ_GOVERNOR_SIMPLE) &&
+		device->pwrctrl.restore_slumber == 0)
 		kgsl_pwrctrl_pwrlevel_change(device,
-					device->pwrctrl.default_pwrlevel);
+					     device->pwrctrl.thermal_pwrlevel);
 }
 
 #define HISTORY_SIZE 10
@@ -174,18 +175,12 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 
 	/* In "performance" mode the clock speed always stays
 	   the same */
+
 	if (priv->governor == TZ_GOVERNOR_PERFORMANCE)
 		return;
 
 	device->ftbl->power_stats(device, &stats);
-	priv->bin.total_time += stats.total_time;
-	priv->bin.busy_time += stats.busy_time;
-	/* Do not waste CPU cycles running this algorithm if
-	 * the GPU just started, or if less than FLOOR time
-	 * has passed since the last run.
-	 */
-	if ((stats.total_time == 0) ||
-		(priv->bin.total_time < FLOOR))
+	if (stats.total_time == 0)
 		return;
 
 	/* If the GPU has stayed in turbo mode for a while, *
@@ -204,14 +199,9 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 		priv->no_switch_cnt = 0;
 	}
 
-	idle = priv->bin.total_time - priv->bin.busy_time;
-	priv->bin.total_time = 0;
-	priv->bin.busy_time = 0;
+	idle = stats.total_time - stats.busy_time;
 	idle = (idle > 0) ? idle : 0;
-	if (priv->governor == TZ_GOVERNOR_SIMPLE)
-		val = simple_governor(device, idle);
-	else
-		val = __secure_tz_entry(TZ_UPDATE_ID, idle, device->id);
+	val = __secure_tz_entry(TZ_UPDATE_ID, idle, device->id);
 	if (val)
 		kgsl_pwrctrl_pwrlevel_change(device,
 				pwr->active_pwrlevel + val);
@@ -230,14 +220,15 @@ static void tz_sleep(struct kgsl_device *device,
 
 	__secure_tz_entry(TZ_RESET_ID, 0, device->id);
 	priv->no_switch_cnt = 0;
-	priv->bin.total_time = 0;
-	priv->bin.busy_time = 0;
 }
 
-#ifdef CONFIG_MSM_SCM
 static int tz_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
 	struct tz_priv *priv;
+
+	/* Trustzone is only valid for some SOCs */
+	if (!(cpu_is_msm8x60() || cpu_is_msm8960() || cpu_is_msm8930()))
+		return -EINVAL;
 
 	priv = pwrscale->priv = kzalloc(sizeof(struct tz_priv), GFP_KERNEL);
 	if (pwrscale->priv == NULL)
@@ -249,12 +240,6 @@ static int tz_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 
 	return 0;
 }
-#else
-static int tz_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
-{
-	return -EINVAL;
-}
-#endif /* CONFIG_MSM_SCM */
 
 static void tz_close(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
