c93f11c55b4d4b54270f0717ae30ed8be2edb5db
spezi77 <spezi77@gmx.com>
2016-02-21 10:46:30 +0100

    Revert "block: support volname for HTC devices with proc/emmc support"
    This reverts commit 5d8c65bd71974c40927a03235960c75a6d073758.

--- fs/partitions/check.c
+++ fs/partitions/check.c
@@ -43,9 +43,6 @@
 extern void md_autodetect_dev(dev_t dev);
 #endif
 
-/* HTC */
-extern const char *get_partition_name_by_num(int partnum);
-
 int warn_no_part = 1; /*This is ugly: should make genhd removable media aware*/
 
 static int (*check_part[])(struct parsed_partitions *) = {
@@ -482,13 +479,6 @@ struct hd_struct *add_partition(struct gendisk *disk, int partno,
 		dev_set_name(pdev, "%sp%d", dname, partno);
 	else
 		dev_set_name(pdev, "%s%d", dname, partno);
-
-	/* HTC */
-	if (!strncmp(dev_name(pdev), "mmcblk0p", 8)) {
-		const char *pname = get_partition_name_by_num(p->partno);
-		if (pname)
-			snprintf(p->info->volname, PARTITION_META_INFO_VOLNAMELTH, pname);
-	}
 
 	device_initialize(pdev);
 	pdev->class = &block_class;
