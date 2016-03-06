21a7cbb014d802c4e7729a3b403cdd56c6b157ec
spezi77 <spezi77@gmx.com>
2016-03-06 15:55:12 +0100

    drivers: gpu: make kgsl fixes and add pm_qos_params
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- drivers/gpu/msm/kgsl_pwrctrl.h
+++ drivers/gpu/msm/kgsl_pwrctrl.h
@@ -52,6 +52,7 @@ struct kgsl_pwrctrl {
 	uint32_t pcl;
 	unsigned int nap_allowed;
 	unsigned int idle_needed;
+	const char *regulator_name;
 	const char *irq_name;
 	s64 time;
 	struct kgsl_busy busy;
